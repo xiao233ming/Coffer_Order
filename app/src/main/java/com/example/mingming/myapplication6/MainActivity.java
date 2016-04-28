f113f6ad39f3d75b5d079a863fee0d1028e7b981
xiao233ming <dianziwanwan@163.com>
2016-04-29 00:02:59 +0800

    update

--- app/src/main/java/com/example/mingming/myapplication6/MainActivity.java
+++ app/src/main/java/com/example/mingming/myapplication6/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.mingming.myapplication6;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = "MainActivity";
     int quantity = 0;
 
     @Override
@@ -36,19 +37,46 @@ public class MainActivity extends Activity {
         quantityTextView.setText("" + number);
     }
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
-    public void submitOrder(View view) {
-        int price = quantity * 5;
-        String priceMessgae = "Total:$" + price + "\nThank you";
-        displayMessage(priceMessgae);
-    }
-
     private void displayMessage(String message) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
     }
+
+    public void submitOrder(View view) {
+        EditText text = (EditText) findViewById(R.id.name_field);
+        String value = text.getText().toString();
+
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        //Log.v(TAG,"Has whipped cream" + hasWhippedCream);
+
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+
+        int price = calculatePrice(hasWhippedCream,hasChocolate);
+        String priceMessgae = createOrderSummary(value,price,hasWhippedCream,hasChocolate);
+        displayMessage(priceMessgae);
+    }
+
+    private String createOrderSummary(String name,int price,boolean addWhippedCream,boolean addchocolateCheckBox) {
+        String priceMessage = "Name:"+name;
+        priceMessage +="\nAdd whipped cream?" + addWhippedCream;
+        priceMessage +="\nAdd chocolate CheckBox?" + addchocolateCheckBox;
+        priceMessage += "\nQuantity:" + quantity;
+        priceMessage += "\nTotal:$" + price;
+        priceMessage += "\nThank you!";
+        return priceMessage;
+    }
+
+    private int calculatePrice(boolean addWhippedCream,boolean addChocolate) {
+        int basePrice = 5;
+        if (addWhippedCream){
+            basePrice = basePrice + 1;
+        }
+        if (addChocolate){
+            basePrice = basePrice + 2;
+        }
+        return quantity * basePrice;
+    }
+
 }
